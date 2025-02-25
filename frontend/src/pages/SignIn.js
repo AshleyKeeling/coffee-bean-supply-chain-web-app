6c3a12d87efe6755378f57ee2307371543c46cb8
Ashley <ashleykeeling2020@gmail.com>
2025-02-25 09:56:00 +0000

    basics of sign in and up page completed

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,8 +1,82 @@
+import { useState } from "react"
+// import { useSignup } from "../Hooks/useSignup";
+
+import coffeeSupplyChain from '../assets/images/coffeeSupplyChain.png';
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
+    const [email, setEmail] = useState('');
+
+    const [password, setPassword] = useState('');
+
+    const [keepSignedIn, setKeepSignedIn] = useState(false);
+
+    // const { signup, error, isLoading } = useSignup();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        // await signup(email, password);
+        alert(`
+            Email: ${email}
+            Password: ${password}
+            Keep Signed In: ${keepSignedIn ? "Yes" : "No"}
+            `);
+    }
+
     return (
-        <div>
-            <p>signIn</p>
-        </div>
+        <div className="main-content row">
+            <h1 className="heading-1-size mt-5">Welcome Back</h1>
+            <form className="col-12 col-md-6" id="sign-up-in-form" onSubmit={handleSubmit}>
+
+                <div>
+                    <label className="heading-4-size">Email</label>
+                    <input
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        className="form-control"
+                        placeholder="e.g garrysmith@gmail.com"
+                    />
+                </div>
+
+                <div>
+                    <label className="heading-4-size">Password</label>
+                    <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        className="form-control"
+                        placeholder="e.g **********"
+                    />
+                </div>
+
+                <div className="d-flex justify-content-between align-items-center">
+                    <fieldset>
+                        <input
+                            type="checkbox"
+                            checked={keepSignedIn}
+                            className="form-check-input"
+                            onChange={(e) => setKeepSignedIn(e.target.checked)}
+                        />
+                        <label className="body-size">Keep me signed in?</label>
+                    </fieldset>
+
+                    <p className="body-size mb-0"><strong>Forgot Your Password?</strong></p>
+                </div>
+
+                <button className="button border primary-bg text-white rounded text-center py-3 w-100">Sign In</button>
+
+                {/* <button disabled={isLoading} className="btn btn-primary mt-2 mb-3">Sign Up</button> */}
+                {/* {error && <div className="error">{error}</div>} */}
+
+                <hr />
+                <span className="d-flex justify-content-center mb-3">Don't have an account? <Link to="/signUp" className="text-decoration-none text-black ms-2"><strong>Sign Up</strong></Link></span>
+            </form>
+            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                <img src={coffeeSupplyChain} alt="" className=" w-100 image-fluid rounded" />
+            </div>
+        </div >
     )
 }
 
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -18,69 +18,89 @@ const SignUp = () => {
         e.preventDefault()
 
         // await signup(email, password);
+        alert(`
+            Email: ${email}
+            Ethereum Address: ${ethereumAddress}
+            Role Type: ${roleType}
+            Password: ${password}
+            Confirm Password: ${confirmPassword}
+            Keep Signed In: ${keepSignedIn ? "Yes" : "No"}
+            `);
     }
 
     return (
         <div className="main-content row">
-            <h1 className="heading-1-size mt-3">Create Account</h1>
-            <form className="col-12 col-md-6" onSubmit={handleSubmit}>
+            <h1 className="heading-1-size mt-5">Create Account</h1>
+            <form className="col-12 col-md-6" id="sign-up-in-form" onSubmit={handleSubmit}>
+                <div>
+                    <label className="heading-4-size">Email</label>
+                    <input
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        className="form-control"
+                        placeholder="e.g garrysmith@gmail.com"
+                    />
+                </div>
 
-                <label>Email</label>
-                <input
-                    type="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    className="form-control"
-                    placeholder="e.g garrysmith@gmail.com"
-                />
+                <div>
+                    <label className="heading-4-size">Ethereum Address</label>
+                    <input
+                        type="text"
+                        onChange={(e) => setEthereumAddress(e.target.value)}
+                        value={ethereumAddress}
+                        className="form-control"
+                        placeholder="e.g 0x1234567890abcdef...."
+                    />
+                </div>
 
-                <label>Ethereum Address</label>
-                <input
-                    type="text"
-                    onChange={(e) => setEthereumAddress(e.target.value)}
-                    value={ethereumAddress}
-                    className="form-control"
-                    placeholder="e.g 0x1234567890abcdef...."
-                />
+                <div>
+                    <label className="heading-4-size">Supply Chain Role</label>
+                    <select value={roleType} onChange={(e) => setRoleType(e.target.value)} className="form-select">
+                        <option value="Manager">Manager</option>
+                        <option value="Farmer">Farmer</option>
+                        <option value="Harvestor">Harvestor</option>
+                        <option value="Processor">Processor</option>
+                        <option value="Drying Specialist<">Drying Specialist</option>
+                        <option value="Exporter">Exporter</option>
+                        <option value="Roaster">Roaster</option>
+                        <option value="Packaging Specialist">Packaging Specialist</option>
+                        <option value="Distributor">Distributor</option>
+                    </select>
+                </div>
 
-                <label>Supply Chain Role</label>
-                <select value={roleType} onChange={(e) => setRoleType(e.target.value)} className="form-control">
-                    <option value="Manager">Manager</option>
-                    <option value="Farmer">Farmer</option>
-                    <option value="Harvestor">Harvestor</option>
-                    <option value="Processor">Processor</option>
-                    <option value="Drying Specialist<">Drying Specialist</option>
-                    <option value="Exporter">Exporter</option>
-                    <option value="Roaster">Roaster</option>
-                    <option value="Packaging Specialist">Packaging Specialist</option>
-                    <option value="Distributor">Distributor</option>
-                </select>
+                <div>
+                    <label className="heading-4-size">Password</label>
+                    <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        className="form-control"
+                        placeholder="e.g **********"
+                    />
+                </div>
 
-                <label>Password</label>
-                <input
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    className="form-control"
-                    placeholder="e.g **********"
-                />
+                <div>
+                    <label className="heading-4-size">Confirm Password</label>
+                    <input
+                        type="password"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        value={confirmPassword}
+                        className="form-control"
+                        placeholder="e.g **********"
+                    />
+                </div>
 
-                <label>Confirm Password</label>
-                <input
-                    type="password"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    value={confirmPassword}
-                    className="form-control"
-                    placeholder="e.g **********"
-                />
+                <div>
+                    <input
+                        type="checkbox"
+                        checked={keepSignedIn}
+                        className="form-check-input"
+                        onChange={(e) => setKeepSignedIn(e.target.checked)}
+                    />
+                    <label className="body-size">keep me signed in?</label>
+                </div>
 
-                <label>keep me signed in?</label>
-                <input
-                    type="checkbox"
-                    checked={keepSignedIn}
-                    className=""
-                    onChange={(e) => setKeepSignedIn(e.target.checked)}
-                />
 
                 <button className="button border primary-bg text-white rounded text-center py-3 w-100">Sign Up</button>
 
@@ -88,7 +108,7 @@ const SignUp = () => {
                 {/* {error && <div className="error">{error}</div>} */}
 
                 <hr />
-                <span className="d-flex justify-content-center">Already have an account? <Link to="/signIn" className="text-decoration-none text-black ms-2"><strong>Sign In</strong></Link></span>
+                <span className="d-flex justify-content-center mb-3">Already have an account? <Link to="/signIn" className="text-decoration-none text-black ms-2"><strong>Sign In</strong></Link></span>
             </form>
             <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
                 <img src={coffeeOutside} alt="" className=" w-100 image-fluid rounded" />

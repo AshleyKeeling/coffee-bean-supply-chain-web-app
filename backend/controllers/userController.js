e7c4674e573f2d53e2d259c8d9727f54f9947364
Ashley <ashleykeeling2020@gmail.com>
2025-02-28 16:41:59 +0000

    created code for user sign in and up for mongoDB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,37 @@
+const User = require('../models/userModel');
 
 
 // sign up
 const signupUser = async (req, res) => {
-    res.status(200).json({ message: "user sign up" })
+    const { email, password, role, ethereum_address } = req.body;
+
+    try {
+        // adds user to database with validation checks
+        const user = await User.signup(email, password, role, ethereum_address);
+        // create JWT token
+
+        res.status(200).json({ message: "user created " })
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
 }
 
 // sign in
 const signinUser = async (req, res) => {
-    res.status(200).json({ message: "user sign in" })
+    const { email, password } = req.body;
+
+    try {
+        // log user in
+        const user = await User.signin(email, password);
+
+        // create JWT token
+
+        res.status(200).json({ message: "user signed in" })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = { signupUser, signinUser };

e6ae371739d99a94ba393685ce9c693a9857eb6d
Ashley <ashleykeeling2020@gmail.com>
2025-03-03 18:11:19 +0000

    create hook for sign in for backend

---
+++ frontend/src/hooks/useSignin.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const useSignin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+
+
+    const signin = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('/api/user/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+
+        if (response.ok) {
+            // save JWT to local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            setIsLoading(false);
+        }
+    }
+}
